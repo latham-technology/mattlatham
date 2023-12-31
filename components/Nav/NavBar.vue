b3ab9b48de6643eec143318c91024f1c790bcbd8
Matt Latham <github@mattlatham.dev>
2023-12-31 04:05:00 -0800

    updates

--- components/Nav/NavBar.vue
+++ components/Nav/NavBar.vue
@@ -1,14 +1,15 @@
 <template>
     <TransitionSlide :offset="[0, '-100%']">
-        <div v-show="showNavBar" class="nav-bar">
+        <div
+            v-show="showNavBar"
+            class="flex flex-wrap gap-5 items-stretch justify-between py-2 z-20"
+        >
             <NavBrand />
 
-            <div class="nav-bar__right-container">
-                <div class="nav-links">
-                    <nuxt-link to="/projects" class="nav-link"
-                        >Projects</nuxt-link
-                    >
-                    <nuxt-link to="/posts" class="nav-link">Posts</nuxt-link>
+            <div class="flex items-center p-2 ml-auto">
+                <div class="nav-links flex items-center ml-auto">
+                    <nuxt-link to="/projects">Projects</nuxt-link>
+                    <nuxt-link to="/posts">Posts</nuxt-link>
                 </div>
 
                 <ColorModePicker class="hidden md:block mx-2" />
@@ -52,39 +53,7 @@ function onScroll() {
 </script>
 
 <style lang="scss" scoped>
-.nav-bar {
-    @apply py-2;
-    @apply flex items-stretch justify-between;
-    @apply z-20;
-
-    &__brand-wrapper {
-        @apply mr-auto h-full;
-    }
-
-    &__brand-link {
-        @apply flex items-center h-full p-2;
-    }
-
-    &__brand-image {
-        @apply p-1 w-16;
-    }
-
-    &__brand-text {
-        @apply hidden md:block;
-        @apply ml-4 font-bold text-2xl whitespace-nowrap;
-        @apply text-brand-primary dark:text-brand-secondary;
-    }
-
-    &__right-container {
-        @apply flex items-center;
-        @apply p-2;
-    }
-}
-
 .nav-links {
-    @apply flex;
-    @apply items-center ml-auto;
-
     a {
         @apply block font-bold tracking-widest px-3 py-2;
         @apply rounded transform transition;
@@ -94,12 +63,6 @@ function onScroll() {
             @apply bg-brand-primary text-black;
             @apply -translate-y-0.5;
         }
-    }
-}
-
-.nav-burger {
-    @screen md {
-        display: none !important;
     }
 }
 </style>
